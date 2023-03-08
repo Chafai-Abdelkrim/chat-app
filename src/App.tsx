294fc933718dc1f621921e4ad498a2dc41499ebc
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-08 12:11:34 +0100

    added firebase files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCHw_RjC_JsrZECeUCVt77jpyy0wqiHUbo",
+  authDomain: "chat-app-61c03.firebaseapp.com",
+  projectId: "chat-app-61c03",
+  storageBucket: "chat-app-61c03.appspot.com",
+  messagingSenderId: "126377294519",
+  appId: "1:126377294519:web:24203a20f63154a225ab32"
+};
+
 function App() {
   return (
     <div className="App">

dd880e7c2feb75e872f55b3f5c0262e4f0897fce
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-09 09:36:28 +0100

    finished App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { collection, getFirestore } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import FirebaseContext from './FirebaseContext';
+import { Chat, Loading } from './components';
 import './styles/App.css';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHw_RjC_JsrZECeUCVt77jpyy0wqiHUbo",
-  authDomain: "chat-app-61c03.firebaseapp.com",
-  projectId: "chat-app-61c03",
-  storageBucket: "chat-app-61c03.appspot.com",
-  messagingSenderId: "126377294519",
-  appId: "1:126377294519:web:24203a20f63154a225ab32"
+  apiKey: 'AIzaSyCHw_RjC_JsrZECeUCVt77jpyy0wqiHUbo',
+  authDomain: 'chat-app-61c03.firebaseapp.com',
+  projectId: 'chat-app-61c03',
+  storageBucket: 'chat-app-61c03.appspot.com',
+  messagingSenderId: '126377294519',
+  appId: '1:126377294519:web:24203a20f63154a225ab32',
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+  const messageCollection = collection(db, 'messages');
+
   return (
-    <div className="App">
-      
-    </div>
+    <FirebaseContext.Provider value={{ auth, db, user, messageCollection }}>
+      <div className="App">{loading ? <Loading /> : <Chat />}</div>
+    </FirebaseContext.Provider>
   );
 }
 

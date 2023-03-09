0c00c7ceb320a5a62f336f6fe148cf574f762c65
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-09 10:43:49 +0100

    added github component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,12 +1,36 @@
+import {
+  CollectionReference,
+  DocumentData,
+  orderBy,
+  query,
+} from 'firebase/firestore';
 import { motion } from 'framer-motion';
+import { useContext, useEffect, useRef } from 'react';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import FirebaseContext from '../FirebaseContext';
 
 const Chat = () => {
+  const { user, messageCollection } = useContext(FirebaseContext);
+  const messageQuery = query(
+    messageCollection as CollectionReference<DocumentData>,
+    orderBy('timestamp', 'asc')
+  );
+  const [messages, loading] = useCollectionData(messageQuery);
+  const bottom = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () =>
+    bottom.current?.scrollIntoView({ behavior: 'smooth' });
 
-    return (
-        <motion.div>
+  useEffect(scrollToBottom, [messages]);
 
-        </motion.div>
-    )
+  return ( 
+    <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1}}
+        className='Chat'
+    >
+        <header></header>
+    </motion.div> 
+  );
 };
 
 export default Chat;

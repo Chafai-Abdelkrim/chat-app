4c8bb82e90a0870a26d48117277cf67d6c49c7d1
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-09 09:55:56 +0100

    added Loading component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,12 @@
-const Chat = () => {};
+import { motion } from 'framer-motion';
+
+const Chat = () => {
+
+    return (
+        <motion.div>
+
+        </motion.div>
+    )
+};
 
 export default Chat;
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,3 +1,11 @@
-const Loading = () => {}
+import { CircularProgress } from 'react-cssfx-loading';
+
+const Loading = () => {
+  return (
+    <div className="Loading">
+      <CircularProgress color="#9922ff" height="60px" width="60px" />
+    </div>
+  );
+};
 
 export default Loading;

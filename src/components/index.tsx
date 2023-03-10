cb4eab016db01afe96cdd62f12ffff33da9eb8c7
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-03-10 10:34:11 +0100

    added Form comp to index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,3 +5,4 @@ export { default as Button } from './Button';
 export { default as SignIn } from './SignIn';
 export { default as SignOut } from './SignOut';
 export { default as Message } from './Message';
+export { default as Form } from './Form';
